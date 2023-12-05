2c9ac0bdf3cb141cd94e2bcc45ba211ef45919fd
Daniel Damkoehler <170141+dandam@users.noreply.github.com>
2023-12-05 13:05:36 -0800

    chapter 05 - user logins

--- app/models.py
+++ app/models.py
@@ -1,15 +1,31 @@
 from datetime import datetime
-from app import db
+from typing import Optional
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import db, login
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
+
+class User(UserMixin, db.Model):
+    id: so.Mapped[int] =so.mapped_column(primary_key=True)
+    username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     
     def __repr__(self):
         return '<User {}>'.format(self.username)
+        
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password, method='pbkdf2:sha256', salt_length=16)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+        
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
